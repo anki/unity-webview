79733531c27f2bc13a967ed572870cac1c8bde67
David Hernandez Cerpa <dcerpa@anki.com>
2017-02-09 07:34:34 -0800

    Updated Android version to 19 so we can check for KITKAT and enable hardware acceleration. Fixed Unity path.

--- plugins/Android/install.sh
+++ plugins/Android/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-UNITYLIBS=`find -L /Applications/Unity | grep classes.jar | tail -1`
+UNITYLIBS=`find -L /Applications/Unity-5.3.5p8 | grep classes.jar | tail -1`
 DSTDIR="../../build/Packager/Assets/Plugins/Android"
 export ANT_OPTS=-Dfile.encoding=UTF8
-android update project -t android-18 -p .
+android update project -t android-19 -p .
 mkdir -p libs
 cp $UNITYLIBS libs
 ant "-Djava.compilerargs=-Xlint:deprecation" release
